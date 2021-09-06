69b083133ea63c8c6a12022abb1958f1b016858c
jinsunee <jsunee.dev@gmail.com>
2021-09-06 16:49:11 +0900

    add: list.model, task.model

---
+++ src/models/list.model.ts
@@ -0,0 +1,65 @@
+import { v4 as UUID } from "uuid";
+
+// Interfaces
+interface IProps {
+  id?: string;
+  name: string;
+}
+
+interface IListInterface extends IProps {
+  timestamp: number;
+}
+
+export default class ListModel {
+  private _id: string;
+  private _name: string;
+
+  constructor({ id = UUID(), name = "" }) {
+    this._id = id;
+    this._name = name;
+  }
+
+  /**
+   * Set Id
+   * @param value
+   */
+  setId(value: string) {
+    this._id = value !== "" ? value : null;
+  }
+
+  /**
+   * Get Id
+   * @return {string|*}
+   */
+  getId() {
+    return this._id;
+  }
+
+  /**
+   * Set Name
+   * @param value
+   */
+  setName(value: string) {
+    this._name = value !== "" ? value : null;
+  }
+
+  /**
+   * Get Name
+   * @return {string|*}
+   */
+  getName() {
+    return this._name;
+  }
+
+  /**
+   * Get Base entity mappings
+   * @return {IListInterface}
+   */
+  getEntityMappings(): IListInterface {
+    return {
+      id: this.getId(),
+      name: this.getName(),
+      timestamp: new Date().getTime(),
+    };
+  }
+}
---
+++ src/models/task.model.ts
@@ -0,0 +1,109 @@
+import { v4 as UUID } from "uuid";
+
+// Interfaces
+interface IProps {
+  id?: string;
+  listId: string;
+  description: string;
+  completed: boolean;
+}
+interface ITaskInterface extends IProps {
+  timestamp: number;
+}
+
+export default class TaskModel {
+  private _id: string;
+  private _listId: string;
+  private _description: string;
+  private _completed: boolean;
+
+  constructor({
+    id = UUID(),
+    listId,
+    description = "",
+    completed = false,
+  }: IProps) {
+    this._id = id;
+    this._listId = listId;
+    this._description = description;
+    this._completed = completed;
+  }
+
+  /**
+   * Set Id
+   * @param value
+   */
+  setId(value: string) {
+    this._id = value !== "" ? value : null;
+  }
+
+  /**
+   * Get Id
+   * @return {string|*}
+   */
+  getId() {
+    return this._id;
+  }
+
+  /**
+   * Set List Id
+   * @param value
+   */
+  setListId(value: string) {
+    this._listId = value !== "" ? value : null;
+  }
+
+  /**
+   * Get List Id
+   * @return {string|*}
+   */
+  getListId() {
+    return this._listId;
+  }
+
+  /**
+   * Set Description
+   * @param value
+   */
+  setDescription(value: string) {
+    this._description = value ? value : null;
+  }
+
+  /**
+   * Get Description
+   * @return {string}
+   */
+  getDescription() {
+    return this._description;
+  }
+
+  /**
+   * Set Completed
+   * @param value
+   */
+  setCompleted(value: boolean) {
+    this._completed = value ? value : null;
+  }
+
+  /**
+   * Get Completed
+   * @return {boolean}
+   */
+  getCompleted() {
+    return this._completed;
+  }
+
+  /**
+   * Get Base entity mappings
+   * @return {ITaskInterface}
+   */
+  getEntityMappings(): ITaskInterface {
+    return {
+      id: this.getId(),
+      listId: this.getListId(),
+      description: this.getDescription(),
+      completed: this.getCompleted(),
+      timestamp: new Date().getTime(),
+    };
+  }
+}
